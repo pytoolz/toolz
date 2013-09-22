204f24c069028db59464b9b4a85cf5a89d5902fe
John Jacobsen <john@mail.npxdesigns.com>
2013-09-22 11:42:36 -0500

    PEP-8

--- toolz/dicttoolz/tests/test_core.py
+++ toolz/dicttoolz/tests/test_core.py
@@ -1,12 +1,16 @@
 from toolz.dicttoolz import merge, valmap, keymap
 
+
 inc = lambda x: x + 1
+
 
 def test_merge():
     assert merge({1: 1, 2: 2}, {3: 4}) == {1: 1, 2: 2, 3: 4}
 
+
 def test_valmap():
     assert valmap(inc, {1: 1, 2: 2}) == {1: 2, 2: 3}
+
 
 def test_keymap():
     assert keymap(inc, {1: 1, 2: 2}) == {2: 1, 3: 2}
