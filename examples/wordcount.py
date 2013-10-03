dfc206dfe72602a9c4d80a32d7480f564afe50c2
Matthew Rocklin <mrocklin@gmail.com>
2013-10-02 20:38:44 -0700

    add wordcount example

---
+++ examples/wordcount.py
@@ -0,0 +1,12 @@
+from toolz import *
+
+def stem(word):
+    """ Stem word to primitive form """
+    return word.lower().rstrip(",.!:;'-\"").lstrip("'\"")
+
+wordcount = comp(frequencies, partial(map, stem), str.split)
+
+if __name__ == '__main__':
+    print(wordcount("This cat jumped over this other cat!"))
+    # prints {'this': 2, 'cat': 2, 'jumped': 1, 'over': 1, 'other': 1}
+
