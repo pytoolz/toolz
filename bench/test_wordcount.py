1dea01612007ce6656ef47943fdb6fc9afd016b4
Matthew Rocklin <mrocklin@gmail.com>
2013-10-06 07:38:20 -0700

    add wordcount on shakespeare benchmark

---
+++ bench/test_wordcount.py
@@ -0,0 +1,17 @@
+from toolz.curried import *
+import os
+
+if not os.path.exists('bench/shakespeare.txt'):
+    os.system('wget http://www.gutenberg.org/ebooks/100.txt.utf-8')
+    os.system('mv 100.txt.utf-8 shakespeare.txt')
+
+
+def stem(word):
+    """ Stem word to primitive form """
+    return word.lower().rstrip(",.!:;'-\"").lstrip("'\"")
+
+wordcount = comp(frequencies, map(stem), concat, map(str.split))
+
+def test_shakespeare():
+    with open('bench/shakespeare.txt') as f:
+        counts = wordcount(f)
