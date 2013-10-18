35e6e376fc698441d8fa2abc29a6713a5a2075dc
Matthew Rocklin <mrocklin@gmail.com>
2013-10-18 09:03:20 -0700

    add iseven doctest to groupby

--- toolz/itertoolz/core.py
+++ toolz/itertoolz/core.py
@@ -49,6 +49,13 @@ def groupby(f, coll):
     >>> names = ['Alice', 'Bob', 'Charlie', 'Dan', 'Edith', 'Frank']
     >>> groupby(len, names)
     {3: ['Bob', 'Dan'], 5: ['Alice', 'Edith', 'Frank'], 7: ['Charlie']}
+
+    >>> iseven = lambda x: x % 2 == 0
+    >>> groupby(iseven, [1, 2, 3, 4, 5, 6, 7, 8])
+    {False: [1, 3, 5, 7], True: [2, 4, 6, 8]}
+
+    See Also:
+        ``countby``
     """
     d = dict()
     for item in coll:
