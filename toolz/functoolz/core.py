478317a330f6bad086c3bee65f881109fafe9bd9
Matthew Rocklin <mrocklin@gmail.com>
2013-10-25 10:52:06 -0700

    add UNIX comment to pipe

--- toolz/functoolz/core.py
+++ toolz/functoolz/core.py
@@ -213,6 +213,11 @@ def pipe(data, *functions):
 
     I.e. ``pipe(data, f, g, h)`` is equivalent to ``h(g(f(data)))``
 
+    We think of the value as progressing through a pipe of several
+    transformations, much like pipes in UNIX
+
+    ``$ cat data | f | g | h``
+
     >>> double = lambda i: 2 * i
     >>> pipe(3, double, str)
     '6'
