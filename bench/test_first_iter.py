a8528bb5f213e3b46a78fd5eb8c9ce51a372a475
Matthew Rocklin <mrocklin@gmail.com>
2013-11-01 18:36:20 -0700

    move iters within tests - was burning up before

--- bench/test_first_iter.py
+++ bench/test_first_iter.py
@@ -1,13 +1,14 @@
 import itertools
 from toolz import first, second
 
-iters = itertools.imap(iter, [(1, 2) for i in range(1000000)])
 
 def test_first_iter():
+    iters = map(iter, [(1, 2) for i in range(1000000)])
     for p in iters:
         first(p)
 
 
 def test_second_iter():
+    iters = map(iter, [(1, 2) for i in range(1000000)])
     for p in iters:
         second(p)
