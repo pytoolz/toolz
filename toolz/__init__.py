6dceaea4cf54436699c73e4e4a88a0c7be75863b
Erik Welch <erik.n.welch@gmail.com>
2013-11-10 11:13:10 -0500

    Add jackknife function (see #66) in the sandbox module (see #67)
    jackknife: repeatedly iterate over seq, each time omitting a successive element
    
    This is the first addition to the sandbox.   The purpose and usage of the
    sandbox still needs to be documented.
    
    The goal for `jackknife` is to eventually be in a `sampling` module, which
    may end up being a separate project.

--- toolz/__init__.py
+++ toolz/__init__.py
@@ -13,6 +13,8 @@ from .dicttoolz import merge, merge_with, keymap, valmap, assoc, update_in
 
 from .compatibility import map, filter
 
+from . import sandbox
+
 from functools import partial, reduce
 
 sorted = sorted
