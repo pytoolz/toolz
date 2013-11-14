8493ec90f7624951dd9e228210e74a29e7c2b6d6
Matthew Rocklin <mrocklin@gmail.com>
2013-11-13 20:10:36 -0800

    fold works with multiprocessing via partial
    Multiprocessing has issues:
    
    Multiprocessing.Pool.map depends on pickle
    Pickle can't handle closures, partial evaluations, or top-level functions
    
    Fortunately multiprocessing.Pool.apply_async *does* allow for explicit
    passing of extra arguments.  So we make our own Pool.map (called mpmap)
    that explicitly unpacks partials (and curries) and injects the tasks
    directly.
    
    We make a basic fold-with-multiprocessing test pass.  Hopefully we can
    find a better way

--- toolz/parallel/core.py
+++ toolz/parallel/core.py
@@ -1,16 +1,39 @@
-from toolz import partition_all
+from toolz import partition_all, partial
+from toolz import reduce as core_reduce
 
 # map is presumed parallel
 
 chunksize = 4
 
+def mpmap(p, func, seq):
+    if hasattr(func, 'args') and hasattr(func, 'keywords'):
+        seq = [func.args + (item,) for item in seq]
+        keywords = func.keywords
+        func = func.func
+    else:
+        seq = ((item,) for item in seq)
+        keywords = {}
+
+    if keywords:
+        results = [p.apply_async(func, args, keywords) for args in seq]
+    else:
+        results = [p.apply_async(func, args) for args in seq]
+
+
+    return map(lambda x: x.get(), results)
+
+
+def reduce(binop, initial, coll):
+    return core_reduce(binop, coll, initial)
+
+
 def fold(binop, seq, default, map=map):
     seq = list(seq)
     if len(seq) < chunksize:
-        return reduce(binop, seq, default)
+        return core_reduce(binop, seq, default)
     else:
         chunks = partition_all(chunksize, seq)
-        results = map(lambda s: reduce(binop, s, default), chunks)
+        results = map(partial(reduce, binop, default), chunks)
         return fold(binop, results, default)
 
 
--- toolz/parallel/tests/test_core.py
+++ toolz/parallel/tests/test_core.py
@@ -1,6 +1,7 @@
-from toolz.parallel.core import fold
+from toolz.parallel.core import fold, mpmap
 from toolz.parallel.core import filter as pfilter
 import operator as op
+from functools import partial
 
 iseven = lambda x: x % 2 == 0
 
@@ -9,5 +10,12 @@ def test_fold():
     print reduce(op.add, range(10), 0)
     assert fold(op.add, range(10), 0) == reduce(op.add, range(10), 0)
 
+
 def test_filter():
     assert list(filter(iseven, range(10))) == list(filter(iseven, range(10)))
+
+
+def test_fold_multiprocessing():
+    from multiprocessing import Pool
+    p = Pool(4)
+    assert fold(op.add, range(10), 0, map=partial(mpmap, p)) == sum(range(10))
