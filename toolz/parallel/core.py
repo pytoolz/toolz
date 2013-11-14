26881facbaee5be6bbb45f4a6c115c96775297e3
Matthew Rocklin <mrocklin@gmail.com>
2013-11-13 16:03:44 -0800

    add lame filter based on map

--- toolz/parallel/core.py
+++ toolz/parallel/core.py
@@ -1,8 +1,10 @@
 from toolz import partition_all
 
+# map is presumed parallel
+
 chunksize = 4
 
-def fold(binop, seq, default):
+def fold(binop, seq, default, map=map):
     seq = list(seq)
     if len(seq) < chunksize:
         return reduce(binop, seq, default)
@@ -11,3 +13,7 @@ def fold(binop, seq, default):
         results = map(lambda s: reduce(binop, s, default), chunks)
         return fold(binop, results, default)
 
+
+def filter(pred, seq, map=map):
+    results = map(lambda x: (pred(x), x), seq)
+    return [x for p, x in results if p]
--- toolz/parallel/tests/test_core.py
+++ toolz/parallel/tests/test_core.py
@@ -1,7 +1,13 @@
 from toolz.parallel.core import fold
+from toolz.parallel.core import filter as pfilter
 import operator as op
+
+iseven = lambda x: x % 2 == 0
 
 def test_fold():
     print fold(op.add, range(10), 0)
     print reduce(op.add, range(10), 0)
     assert fold(op.add, range(10), 0) == reduce(op.add, range(10), 0)
+
+def test_filter():
+    assert list(filter(iseven, range(10))) == list(filter(iseven, range(10)))
