5bf9f1d5e91ebbaf1378e5be2d601c269ac984f2
Erik Welch <erik.n.welch@gmail.com>
2013-11-19 08:34:54 -0500

    oops pep8

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setup(name='toolz',
                 'toolz.functoolz',
                 'toolz.dicttoolz',
                 'toolz.sandbox'],
-      long_description=open('README.rst').read() if exists("README.rst") else "",
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
       zip_safe=False)
