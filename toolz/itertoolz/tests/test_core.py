d41ddd55b87283a87a0105f4cfb1b98880d8ab22
Erik Welch <erik.n.welch@gmail.com>
2013-11-25 22:47:19 -0500

    Fix OverflowError from tests on 32bit systems.

--- toolz/itertoolz/tests/test_core.py
+++ toolz/itertoolz/tests/test_core.py
@@ -167,7 +167,7 @@ def test_concatv():
 
 
 def test_interpose():
-    assert "a" == first(rest(interpose("a", range(10000000000))))
+    assert "a" == first(rest(interpose("a", range(1000000000))))
     assert "tXaXrXzXaXn" == "".join(interpose("X", "tarzan"))
     assert list(interpose(0, itertools.repeat(1, 4))) == [1, 0, 1, 0, 1, 0, 1]
     assert list(interpose('.', ['a', 'b', 'c'])) == ['a', '.', 'b', '.', 'c']
