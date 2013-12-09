af1d4e51f0a1fa78171f643744fa0c1ddf67de75
Matthew Rocklin <mrocklin@gmail.com>
2013-12-09 15:59:49 -0800

    test curry can be serialized

--- toolz/tests/test_serialization.py
+++ toolz/tests/test_serialization.py
@@ -5,3 +5,8 @@ def test_compose():
     f = compose(str, sum)
     g = pickle.loads(pickle.dumps(f))
     assert f((1, 2)) == g((1, 2))
+
+def test_curry():
+    f = curry(map)(str)
+    g = pickle.loads(pickle.dumps(f))
+    assert list(f((1, 2, 3))) == list(g((1, 2, 3)))
