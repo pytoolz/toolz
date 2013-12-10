38ec7ed5784db8e45971a34e3c68a140b2ea8daa
Matthew Rocklin <mrocklin@gmail.com>
2013-12-10 12:20:01 -0800

    add multiprocessing test

--- toolz/tests/test_serialization.py
+++ toolz/tests/test_serialization.py
@@ -12,3 +12,15 @@ def test_curry():
     f = curry(map)(str)
     g = pickle.loads(pickle.dumps(f))
     assert list(f((1, 2, 3))) == list(g((1, 2, 3)))
+
+
+def test_compose_multiprocessing():
+    from operator import add, mul
+    inc = partial(add, 1)
+    double = partial(mul, 2)
+    f = compose(double, inc)
+
+    import multiprocessing as mp
+    p = mp.Pool(4)
+
+    assert p.map(f, [1, 2, 3, 4], chunksize=1) == [4, 6, 8, 10]
