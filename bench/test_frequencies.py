d571cb97ece846e507457a9594c5419fbdbe14ab
Matthew Rocklin <mrocklin@gmail.com>
2014-01-18 19:45:25 -0800

    add no-repeat frequencies benchmark

--- bench/test_frequencies.py
+++ bench/test_frequencies.py
@@ -1,6 +1,12 @@
 from toolz import frequencies, identity
 
-data = range(1000)*1000
+big_data = range(1000)*1000
+small_data = range(100)
+
 
 def test_frequencies():
-    frequencies(data)
+    frequencies(big_data)
+
+def test_frequencies_small():
+    for i in range(1000):
+        frequencies(small_data)
