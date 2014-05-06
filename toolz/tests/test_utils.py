c36f8242df174213299b2cf23c8a26d9745f87a5
Erik Welch <erik.n.welch@gmail.com>
2014-05-05 20:12:16 -0400

    Add `toolz.utils.consume` to efficiently consume an iterator

--- toolz/tests/test_utils.py
+++ toolz/tests/test_utils.py
@@ -1,6 +1,14 @@
-from toolz.utils import raises
+from toolz.utils import raises, consume
 
 
 def test_raises():
     assert raises(ZeroDivisionError, lambda: 1 / 0)
     assert not raises(ZeroDivisionError, lambda: 1)
+
+
+def test_consume():
+    data = [1, 2, 3, 4]
+    it = iter(data)
+    assert consume(it) is None
+    assert raises(StopIteration, lambda: next(it))
+    assert consume(data) is None
