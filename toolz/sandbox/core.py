6dfb2ff7bd2145b9be2441e391c4f4c9d9761619
Erik Welch <erik.n.welch@gmail.com>
2014-05-08 15:51:51 -0400

    Refinements to `EqualityHashKey` including requiring a `key` argument.
    The canonical purpose of `key` is to calculate a partial key hash that is
    good enough.  This is *much* better than using the default hash value.
    
    `'__default__hashkey__'` is used as the default hash value, not a random number.
    
    Many implementation questions and discussion were added as comments to
    facilitate development.

--- toolz/sandbox/core.py
+++ toolz/sandbox/core.py
@@ -1,18 +1,62 @@
-import random
-import sys
-
-# preserve random state
-_randomstate = random.getstate()
 
 
 # See #166: https://github.com/pytoolz/toolz/issues/166
-
+# See #173: https://github.com/pytoolz/toolz/pull/173
+#
+# Qustions:
+#   - What should be allowable as a `key`?
+#       - **CURRENT**: all of the below
+#       - A callable that calculates a key to hash given `item`
+#           - Should the default key be used if the callable returns `None`?
+#               - **CURRENT**: raise TypeError in `__hash__`
+#           - What if the callable returns an unhashable object?
+#               - **CURRENT**: raise TypeError in `__hash__`
+#       - An object to use as a key to hash
+#           - What if the object is unhashable?
+#               - **CURRENT**: raise TypeError (if not None) in `__hash__`
+#       - `None` to specify the default key should be used
+#
+#   - Should `key` be a positional or keyword argument?
+#       - **CURRENT**: positional argument
+#       - Erik: I like positional for many reasons:
+#           - Easiest to use with curry
+#           - Using the inefficient default key must be explicit, which is
+#             much better than using it by default if `key` isn't provided
+#           - I don't like keyword arguments :)
+#
+#   - Should the hash value be calculated in `__hash__` or `__init__`?
+#       - **CURRENT**: in `__hash__`
+#       - `__hash__`:
+#           - this is overall cleaner
+#           - expected behavior (probably)
+#           - `key` attribute is understandable and usable
+#       - `__init__`:
+#           - precalculated hash for faster performance of repeated hashing
+#           - hash value may be used in `__eq__` instead of `_default_hashkey`
+#           - find out during object creation if the provided key is unhashable
+#
+#   - Should `DefaultHashKey` in the docstring be renamed to
+#     `EqualityHashDefault`?
+#       - This can perhaps lead to clearer naming conventions such as:
+#           - EqualityHashLen, EqualityHashType, EqualityHashId, and
+#             EqualityHashFirst
+#
+# Todo:
+#   - Document `key` argument
+#       - We can probably remove the refined `unique(unique(...))` recipe
+#   - Test various uses of `key` argument
+#   - Test other behaviors as they are finalized
+#   - Add technical discussion as code comments describing how and why
+#     this works
+#
 class EqualityHashKey(object):
     """ Create a hash key that uses equality comparisons between items.
 
     This may be used to create hash keys for otherwise unhashable types:
 
-    >>> set(map(EqualityHashKey, [[], [], [1], [1], {}]))  # doctest: +SKIP
+    >>> from toolz import curry
+    >>> DefaultHashKey = curry(EqualityHashKey, None)
+    >>> set(map(DefaultHashKey, [[], [], [1], [1], {}]))  # doctest: +SKIP
     {=[]=, =[1]=, ={}=}
 
     **Caution:** adding N ``EqualityHashKey`` items to a hash container
@@ -26,20 +70,20 @@ class EqualityHashKey(object):
 
     >>> from toolz import unique
     >>> vals = [[], [], [1], [1], [2], {}, {}, {}]
-    >>> list(unique(vals, key=EqualityHashKey))
+    >>> list(unique(vals, key=DefaultHashKey))
     [[], [1], [2], {}]
 
     The above example may be refined if many duplicate items are expected.
     This recipe efficiently filters out duplicate items based on id:
 
-    >>> list(unique(unique(vals, key=id), key=EqualityHashKey))
+    >>> list(unique(unique(vals, key=id), key=DefaultHashKey))
     [[], [1], [2], {}]
 
     **Warning:** don't change the equality value of an item already in a hash
     containter.  Unhashable types are unhashable for a reason.  For example:
 
     >>> L1 = [1] ; L2 = [2]
-    >>> s = set(map(EqualityHashKey, [L1, L2]))
+    >>> s = set(map(DefaultHashKey, [L1, L2]))
     >>> print(s)  # doctest: +SKIP
     set([=[1]=, =[2]=])
 
@@ -54,18 +98,25 @@ class EqualityHashKey(object):
     See Also:
         identity
     """
-    __slots__ = 'item'
-    _hashvalue = random.randint(-sys.maxsize-1, sys.maxsize)
+    __slots__ = ['item', 'key']
+    _default_hashkey = '__default__hashkey__'
 
-    def __init__(self, item):
+    def __init__(self, key, item):
+        if key is None:
+            self.key = self._default_hashkey
+        else:
+            self.key = key
         self.item = item
 
     def __hash__(self):
-        return self._hashvalue
+        if callable(self.key):
+            val = self.key(self.item)
+            return hash(val)
+        return hash(self.key)
 
     def __eq__(self, other):
         try:
-            return (self._hashvalue == other._hashvalue and
+            return (self._default_hashkey == other._default_hashkey and
                     self.item == other.item)
         except AttributeError:
             return False
@@ -78,7 +129,3 @@ class EqualityHashKey(object):
 
     def __repr__(self):
         return '=%s=' % repr(self.item)
-
-
-# preserve random state
-random.setstate(_randomstate)
--- toolz/sandbox/tests/test_core.py
+++ toolz/sandbox/tests/test_core.py
@@ -1,34 +1,55 @@
+from toolz import curry, unique, first
 from toolz.sandbox.core import EqualityHashKey
 
 
-def test_EqualityHashKey():
+def test_EqualityHashKey_default_key():
+    # Is `EqualityHashDefault` a better name than `DefaultHashKey`?
+    DefaultHashKey = curry(EqualityHashKey, None)
     L1 = [1]
     L2 = [2]
     data1 = [L1, L1, L2, [], [], [1], [2], {}, ()]
-    set1 = set(map(EqualityHashKey, data1))
-    set2 = set(map(EqualityHashKey, [[], [1], [2], {}, ()]))
+    set1 = set(map(DefaultHashKey, data1))
+    set2 = set(map(DefaultHashKey, [[], [1], [2], {}, ()]))
     assert set1 == set2
     assert len(set1) == 5
 
-    # Test that ``EqualityHashKey(item)`` is distinct from ``item``
+    # Test that ``DefaultHashKey(item)`` is distinct from ``item``
     T0 = ()
     T1 = (1,)
-    data2 = list(map(EqualityHashKey, [T0, T0, T1, T1, (), (1,)]))
+    data2 = list(map(DefaultHashKey, [T0, T0, T1, T1, (), (1,)]))
     data2.extend([T0, T1, (), (1,)])
     set3 = set(data2)
-    assert set3 == set([(), (1,), EqualityHashKey(()), EqualityHashKey((1,))])
+    assert set3 == set([(), (1,), DefaultHashKey(()), DefaultHashKey((1,))])
     assert len(set3) == 4
-    assert EqualityHashKey(()) in set3
-    assert EqualityHashKey((1,)) in set3
+    assert DefaultHashKey(()) in set3
+    assert DefaultHashKey((1,)) in set3
 
     # Miscellaneous
-    E1 = EqualityHashKey(L1)
-    E2 = EqualityHashKey(L2)
+    E1 = DefaultHashKey(L1)
+    E2 = DefaultHashKey(L2)
     assert str(E1) == '=[1]='
     assert repr(E1) == '=[1]='
     assert E1 != E2
     assert not (E1 == E2)
-    assert E1 == EqualityHashKey(L1)
-    assert not (E1 != EqualityHashKey(L1))
+    assert E1 == DefaultHashKey(L1)
+    assert not (E1 != DefaultHashKey(L1))
     assert E1 != L1
     assert not (E1 == L1)
+
+
+def test_EqualityHashKey_callable_key():
+    # Common simple hash key functions.  Should these be renamed to
+    # EqualityHashLen, EqualityHashType, EqualityHashId, and EqualityHashFirst?
+    LenHashKey = curry(EqualityHashKey, len)
+    TypeHashKey = curry(EqualityHashKey, type)
+    IdHashKey = curry(EqualityHashKey, id)
+    FirstHashKey = curry(EqualityHashKey, first)
+    data1 = [[], [1], (), (1,), {}, {1: 2}]
+    data2 = [[1, 2], (1, 2), (1, 3), [1, 3], [2, 1], {1: 2}]
+    assert list(unique(data1*3, key=LenHashKey)) == data1
+    assert list(unique(data2*3, key=LenHashKey)) == data2
+    assert list(unique(data1*3, key=TypeHashKey)) == data1
+    assert list(unique(data2*3, key=TypeHashKey)) == data2
+    assert list(unique(data1*3, key=IdHashKey)) == data1
+    assert list(unique(data2*3, key=IdHashKey)) == data2
+    assert list(unique(data2*3, key=FirstHashKey)) == data2
