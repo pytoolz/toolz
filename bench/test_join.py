9070fe7b4626d05405550a40c97586c4363ebe92
Matthew Rocklin <mrocklin@gmail.com>
2014-05-24 07:56:57 -0700

    add join benchmarks

---
+++ bench/test_join.py
@@ -0,0 +1,31 @@
+from toolz.curried import *
+import random
+
+
+def burn(seq):
+    for item in seq:
+        pass
+
+
+small = [(i, str(i)) for i in range(100)] * 10
+big = pipe([110]*10000, map(range), concat, list)
+
+
+def test_many_to_many_large():
+    burn(join(get(0), identity, small, big))
+
+
+def test_one_to_one_tiny():
+    A = list(range(20))
+    B = A[::2] + A[1::2][::-1]
+
+    for i in xrange(50000):
+        burn(join(identity, identity, A, B))
+
+
+def test_one_to_many():
+    A = list(range(20))
+    B = pipe([20]*1000, map(range), concat, list)
+
+    for i in xrange(100):
+        burn(join(identity, identity, A, B))
