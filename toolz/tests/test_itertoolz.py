18951a78bffe72620f1f642b030bfe9f0f99fdb3
Matthew Rocklin <mrocklin@gmail.com>
2014-05-25 10:18:40 -0700

    pep8

--- toolz/tests/test_itertoolz.py
+++ toolz/tests/test_itertoolz.py
@@ -292,14 +292,14 @@ def test_join():
 
 def test_key_as_getter():
     squares = [(i, i**2) for i in range(5)]
-    pows =    [(i, i**2, i**3) for i in range(5)]
+    pows = [(i, i**2, i**3) for i in range(5)]
 
-    assert list(join(0, squares, 0, pows)) ==\
-           list(join(lambda x: x[0], squares, lambda x: x[0], pows))
+    assert set(join(0, squares, 0, pows)) == set(join(lambda x: x[0], squares,
+                                                      lambda x: x[0], pows))
 
-    assert list(join([0, 1], squares, [0, 1], pows)) ==\
-           list(join(lambda x: (x[0], x[1]), squares,
-                     lambda x: (x[0], x[1]), pows))
+    get = lambda x: (x[0], x[1])
+    assert set(join([0, 1], squares, [0, 1], pows)) == set(join(get, squares,
+                                                                get, pows))
 
 
 def test_join_double_repeats():
