e816a07c20d95f8f2ff041ff0c988a7110c0f87c
Erik Welch <erik.n.welch@gmail.com>
2014-06-19 21:18:05 -0400

    Document key argument and remove comment questions

--- toolz/sandbox/core.py
+++ toolz/sandbox/core.py
@@ -2,94 +2,52 @@
 
 # See #166: https://github.com/pytoolz/toolz/issues/166
 # See #173: https://github.com/pytoolz/toolz/pull/173
-#
-# Qustions:
-#   - What should be allowable as a `key`?
-#       - **CURRENT**: all of the below
-#       - A callable that calculates a key to hash given `item`
-#           - Should the default key be used if the callable returns `None`?
-#               - **CURRENT**: raise TypeError in `__hash__`
-#           - What if the callable returns an unhashable object?
-#               - **CURRENT**: raise TypeError in `__hash__`
-#       - An object to use as a key to hash
-#           - What if the object is unhashable?
-#               - **CURRENT**: raise TypeError (if not None) in `__hash__`
-#       - `None` to specify the default key should be used
-#
-#   - Should `key` be a positional or keyword argument?
-#       - **CURRENT**: positional argument
-#       - Erik: I like positional for many reasons:
-#           - Easiest to use with curry
-#           - Using the inefficient default key must be explicit, which is
-#             much better than using it by default if `key` isn't provided
-#           - I don't like keyword arguments :)
-#
-#   - Should the hash value be calculated in `__hash__` or `__init__`?
-#       - **CURRENT**: in `__hash__`
-#       - `__hash__`:
-#           - this is overall cleaner
-#           - expected behavior (probably)
-#           - `key` attribute is understandable and usable
-#       - `__init__`:
-#           - precalculated hash for faster performance of repeated hashing
-#           - hash value may be used in `__eq__` instead of `_default_hashkey`
-#           - find out during object creation if the provided key is unhashable
-#
-#   - Should `DefaultHashKey` in the docstring be renamed to
-#     `EqualityHashDefault`?
-#       - This can perhaps lead to clearer naming conventions such as:
-#           - EqualityHashLen, EqualityHashType, EqualityHashId, and
-#             EqualityHashFirst
-#
-# Todo:
-#   - Document `key` argument
-#       - We can probably remove the refined `unique(unique(...))` recipe
-#   - Test various uses of `key` argument
-#   - Test other behaviors as they are finalized
-#   - Add technical discussion as code comments describing how and why
-#     this works
-#
 class EqualityHashKey(object):
     """ Create a hash key that uses equality comparisons between items.
 
     This may be used to create hash keys for otherwise unhashable types:
 
     >>> from toolz import curry
-    >>> DefaultHashKey = curry(EqualityHashKey, None)
-    >>> set(map(DefaultHashKey, [[], [], [1], [1], {}]))  # doctest: +SKIP
-    {=[]=, =[1]=, ={}=}
+    >>> EqualityHashDefault = curry(EqualityHashKey, None)
+    >>> set(map(EqualityHashDefault, [[], (), [1], [1]]))  # doctest: +SKIP
+    {=[]=, =()=, =[1]=}
 
     **Caution:** adding N ``EqualityHashKey`` items to a hash container
-    requires O(N**2) operations, not O(N) as for typical hashable types.
+    may require O(N**2) operations, not O(N) as for typical hashable types.
     Therefore, a suitable key function such as ``tuple`` or ``frozenset``
-    is usually preferred if possible.
+    is usually preferred over using ``EqualityHashKey`` if possible.
 
-    ``EqualityHashKey`` *is* convenient to use, however, especially when
-    a suitable key function is complicated or unavailable.  For example,
-    the following returns all unique values based on equality:
+    The ``key`` argument to ``EqualityHashKey`` should be a function or
+    index that returns a hashable object that effectively distinguishes
+    unequal items.  This helps avoid the poor scaling that occurs when
+    using the default key.  For example, the above example can be improved
+    by using a key function that distinguishes items by length or type:
+
+    >>> EqualityHashLen = curry(EqualityHashKey, len)
+    >>> EqualityHashType = curry(EqualityHashKey, type)  # this works too
+    >>> set(map(EqualityHashLen, [[], (), [1], [1]]))  # doctest: +SKIP
+    {=[]=, =()=, =[1]=}
+
+    ``EqualityHashKey`` is convenient to use when a suitable key function
+    is complicated or unavailable.  For example, the following returns all
+    unique values based on equality:
 
     >>> from toolz import unique
-    >>> vals = [[], [], [1], [1], [2], {}, {}, {}]
-    >>> list(unique(vals, key=DefaultHashKey))
-    [[], [1], [2], {}]
-
-    The above example may be refined if many duplicate items are expected.
-    This recipe efficiently filters out duplicate items based on id:
-
-    >>> list(unique(unique(vals, key=id), key=DefaultHashKey))
-    [[], [1], [2], {}]
+    >>> vals = [[], [], (), [1], [1], [2], {}, {}, {}]
+    >>> list(unique(vals, key=EqualityHashDefault))
+    [[], (), [1], [2], {}]
 
     **Warning:** don't change the equality value of an item already in a hash
     containter.  Unhashable types are unhashable for a reason.  For example:
 
     >>> L1 = [1] ; L2 = [2]
-    >>> s = set(map(DefaultHashKey, [L1, L2]))
-    >>> print(s)  # doctest: +SKIP
-    set([=[1]=, =[2]=])
+    >>> s = set(map(EqualityHashDefault, [L1, L2]))
+    >>> s  # doctest: +SKIP
+    {=[1]=, =[2]=}
 
     >>> L1[0] = 2  # Don't do this!  ``s`` now has duplicate items!
-    >>> print(s)  # doctest: +SKIP
-    set([=[2]=, =[2]=])
+    >>> s  # doctest: +SKIP
+    {=[2]=, =[2]=}
 
     Although this may appear problematic, immutable data types is a common
     idiom in functional programming, and``EqualityHashKey`` easily allows
@@ -104,6 +62,8 @@ class EqualityHashKey(object):
     def __init__(self, key, item):
         if key is None:
             self.key = self._default_hashkey
+        # elif not callable(key):
+        #     self.key = getter(key)
         else:
             self.key = key
         self.item = item
