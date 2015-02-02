1f11a53b6fa09ad613e51b4979e4849c77d8d94b
Matthew Rocklin <mrocklin@gmail.com>
2015-02-02 11:08:55 -0800

    compose takes name kwarg.
    Complement defines name as not_...

--- toolz/functoolz.py
+++ toolz/functoolz.py
@@ -360,10 +360,11 @@ class Compose(object):
     See Also:
         compose
     """
-    __slots__ = ['funcs']
+    __slots__ = ['funcs', '_name']
 
-    def __init__(self, *funcs):
+    def __init__(self, *funcs, **kwargs):
         self.funcs = funcs
+        self._name = kwargs.get('name', None)
 
     def __call__(self, *args, **kwargs):
         fns = list(reversed(self.funcs))
@@ -380,10 +381,10 @@ class Compose(object):
 
     @property
     def __name__(self):
-        return '_o_'.join([fn.__name__ for fn in self.funcs])
+        return self._name or '_o_'.join([fn.__name__ for fn in self.funcs])
 
 
-def compose(*funcs):
+def compose(*funcs, **kwargs):
     """ Compose functions to operate in series.
 
     Returns a function that applies other functions in sequence.
@@ -405,7 +406,7 @@ def compose(*funcs):
     if len(funcs) == 1:
         return funcs[0]
     else:
-        return Compose(*funcs)
+        return Compose(*funcs, name=kwargs.get('name', None))
 
 
 def pipe(data, *funcs):
@@ -445,7 +446,7 @@ def complement(func):
     >>> isodd(2)
     False
     """
-    return compose(operator.not_, func)
+    return compose(operator.not_, func, name='not_' + func.__name__)
 
 
 class juxt(object):
