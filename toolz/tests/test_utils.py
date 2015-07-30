2e961b92051e29f83d2325818c34bd16ac1665a4
llllllllll <joe@quantopian.com>
2015-07-30 14:25:30 -0400

    TST: Adds tests for no_default

--- toolz/tests/test_utils.py
+++ toolz/tests/test_utils.py
@@ -1,6 +1,14 @@
-from toolz.utils import raises
+from toolz.utils import raises, no_default
 
 
 def test_raises():
     assert raises(ZeroDivisionError, lambda: 1 / 0)
     assert not raises(ZeroDivisionError, lambda: 1)
+
+
+def test_no_default_singleton():
+    assert raises(TypeError, type(no_default))
+
+
+def test_no_default_repr():
+    assert str(no_default) == repr(no_default) == '<object no_default>'
