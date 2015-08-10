bfc45c569935086007f10e3ed64a39216e195b79
llllllllll <joe@quantopian.com>
2015-08-09 21:24:23 -0400

    MAINT: no fancy no_default

--- toolz/utils.py
+++ toolz/utils.py
@@ -6,11 +6,4 @@ def raises(err, lamda):
         return True
 
 
-@object.__new__
-class no_default(object):
-    def __new__(self):
-        raise TypeError("cannot create 'no_default' instances")
-
-    def __str__(self):
-        return '<object no_default>'
-    __repr__ = __str__
+no_default = '__no__default__'
