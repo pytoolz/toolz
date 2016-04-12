0b39138ad6d1b2c0964068665bdbd85e3d9cccb4
Steven Cutting <steven.e.cutting@linux.com>
2016-04-11 21:15:40 -0500

    Remove example with random result.

--- toolz/itertoolz.py
+++ toolz/itertoolz.py
@@ -912,19 +912,13 @@ def random_sample(prob, seq, random_state=None):
     Returns a lazy iterator of random items from seq.
 
     ``random_sample`` considers each item independently and without
-    replacement. See below how the first time it returned 13 items and the
-    next time it returned 6 items.
-
-    >>> seq = list(range(100))
-    >>> list(random_sample(0.1, seq))
-    [6, 9, 19, 35, 45, 50, 58, 62, 68, 72, 78, 86, 95]
-    >>> list(random_sample(0.1, seq))
-    [6, 44, 54, 61, 69, 94]
+    replacement.
 
     Providing an integer seed for ``random_state`` will result in deterministic
     sampling. Given the same seed it will return the same sample
     every time.
 
+    >>> seq = list(range(100))
     >>> list(random_sample(0.1, seq, random_state=2016))
     [7, 9, 19, 25, 30, 32, 34, 48, 59, 60, 81, 98]
     >>> list(random_sample(0.1, seq, random_state=2016))
