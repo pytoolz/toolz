9ff94a33508eed0556a5ad801e1fdd00ae84ef42
Jim Crist <crist042@umn.edu>
2016-10-03 11:18:11 -0500

    Add tests for pluck with default, non-None
    Needed for fix in cytoolz (pluck with default would fail for non-None
    defaults).

--- toolz/tests/test_itertoolz.py
+++ toolz/tests/test_itertoolz.py
@@ -328,11 +328,10 @@ def test_pluck():
 
     data = [{'id': 1, 'name': 'cheese'}, {'id': 2, 'name': 'pies', 'price': 1}]
     assert list(pluck('id', data)) == [1, 2]
-    assert list(pluck('price', data, None)) == [None, 1]
+    assert list(pluck('price', data, 0)) == [0, 1]
     assert list(pluck(['id', 'name'], data)) == [(1, 'cheese'), (2, 'pies')]
     assert list(pluck(['name'], data)) == [('cheese',), ('pies',)]
-    assert list(pluck(['price', 'other'], data, None)) == [(None, None),
-                                                           (1, None)]
+    assert list(pluck(['price', 'other'], data, 0)) == [(0, 0), (1, 0)]
 
     assert raises(IndexError, lambda: list(pluck(1, [[0]])))
     assert raises(KeyError, lambda: list(pluck('name', [{'id': 1}])))
