a6665b1ea02c266c4412d1a310a300994e953409
Ryan Grout <ryan@ryangrout.org>
2018-10-18 16:04:07 -0500

    Make dissoc smarter.
    Dissoc was really fast when the keys to remove were relatively few
    compared to the size of the dictionary.  This version will pick the smaller
    container to iterate over, either building up a new dictionary or tearing
    down a copy.
    
    A small heuristic is used to decide if the overhead of building a set is
    worth the effort.  My rudimentry testing shows that if the number of keys is
    less than 60% the length of the dictionary, tearing down a copy is faster.
    If keys is very large, we calculate the intersection between keys and mapping
    and build a new dictionary out of that intersection.

--- toolz/dicttoolz.py
+++ toolz/dicttoolz.py
@@ -197,7 +197,7 @@ def assoc(d, key, value, factory=dict):
     return d2
 
 
-def dissoc(d, *keys):
+def dissoc(d, *keys, **kwargs):
     """ Return a new dict with the given key(s) removed.
 
     New dict has d[key] deleted for each supplied key.
@@ -210,11 +210,20 @@ def dissoc(d, *keys):
     >>> dissoc({'x': 1}, 'y') # Ignores missing keys
     {'x': 1}
     """
-    d2 = copy.copy(d)
-    for key in keys:
-        if key in d2:
-            del d2[key]
-    return d2
+    factory = _get_factory(dissoc, kwargs)
+    d2 = factory()
+
+    if len(keys) < len(d) * .6:
+        d2.update(d)
+        for key in keys:
+            if key in d2:
+                del d2[key]
+    else:
+        remaining = set(d)
+        remaining.difference_update(keys)
+        for k in remaining:
+            d2[k] = d[k]
+        return d2
 
 
 def assoc_in(d, keys, value, factory=dict):
