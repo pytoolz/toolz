9e97f73fee5fc68bd03adcee3b520593ed5063d6
Sam Frances <sam@samfrances.uk>
2019-06-30 14:22:33 +0100

    Write pipeline function, with tests
    This involved some refactoring of the tests for compose(), so that
    we can programmatically ensure that compose() and pipeline()
    have equivalent tests.

--- toolz/functoolz.py
+++ toolz/functoolz.py
@@ -537,6 +537,7 @@ def compose(*funcs):
     '4'
 
     See Also:
+        pipeline
         pipe
     """
     if not funcs:
@@ -545,6 +546,27 @@ def compose(*funcs):
         return funcs[0]
     else:
         return Compose(funcs)
+
+
+def pipeline(*funcs):
+    """ Compose functions to operate in series.
+
+    Returns a function that applies other functions in sequence.
+
+    Functions are applied from left to right so that
+    ``mkpipe(f, g, h)(x, y)`` is the same as ``h(g(f(x, y)))``.
+
+    If no arguments are provided, the identity function (f(x) = x) is returned.
+
+    >>> inc = lambda i: i + 1
+    >>> compose(inc, str)(3)
+    '4'
+
+    See Also:
+        compose
+        pipe
+    """
+    return compose(*reversed(funcs))
 
 
 def pipe(data, *funcs):
