15175067348ac6c531a0343d710eac44f356f362
Ryan Grout <ryan@ryangrout.org>
2020-03-16 15:08:45 -0500

    Remove compatibility tests
    These tests are testing features of Python 3.
    We test that the import works.

--- toolz/tests/test_compatibility.py
+++ toolz/tests/test_compatibility.py
@@ -1,16 +1,1 @@
-
-def test_map_filter_are_lazy():
-    def bad(x):
-        raise Exception()
-    map(bad, [1, 2, 3])
-    filter(bad, [1, 2, 3])
-
-
-def test_dict_iteration():
-    d = {'a': 1, 'b': 2, 'c': 3}
-    assert not isinstance(d.items(), list)
-    assert not isinstance(d.keys(), list)
-    assert not isinstance(d.values(), list)
-    assert set(d.items()) == set(d.items())
-    assert set(d.keys()) == set(d.keys())
-    assert set(d.values()) == set(d.values())
+import toolz.compatibility
