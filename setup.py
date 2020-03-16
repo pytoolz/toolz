cb345f75b35f3704aedcee01b7b279c263a53c88
Ryan Grout <ryan@ryangrout.org>
2020-03-16 15:12:40 -0500

    Support Python >=3.5
    Update setup.py to reflect current Python support.

--- setup.py
+++ setup.py
@@ -21,15 +21,15 @@ setup(name='toolz',
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       zip_safe=False,
-      python_requires=">=3.4",
+      python_requires=">=3.5",
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "License :: OSI Approved :: BSD License",
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
           "Programming Language :: Python :: Implementation :: CPython",
           "Programming Language :: Python :: Implementation :: PyPy"])
