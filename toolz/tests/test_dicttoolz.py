bc3611cb3739a987eb3251bc2e1a8c13d6ea6a8b
Ryan Grout <ryan@ryangrout.org>
2020-06-24 14:18:56 -0500

    Add intersect to test suite.

--- toolz/tests/test_dicttoolz.py
+++ toolz/tests/test_dicttoolz.py
@@ -3,7 +3,7 @@ from collections.abc import Mapping
 import os
 from toolz.dicttoolz import (merge, merge_with, valmap, keymap, update_in,
                              assoc, dissoc, keyfilter, valfilter, itemmap,
-                             itemfilter, assoc_in)
+                             itemfilter, assoc_in, intersect)
 from toolz.functoolz import identity
 from toolz.utils import raises
 
